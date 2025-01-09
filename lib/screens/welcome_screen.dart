52d170dcfa7ae204980119715e37b0adeb557f7e
dell <habibahashemi5@gmail.com>
2025-01-09 12:31:11 -0800

    custom abimation

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -11,79 +11,106 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation animation;
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    animation = ColorTween(
+      begin: Colors.yellow.shade800,
+      end: kBackgroundColor,
+    ).animate(controller);
+    controller.forward();
+
+    controller.addListener(() {
+      print(animation.value);
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Hero(
-                    tag: 'logo',
-                    child: SizedBox(
-                      height: 60,
-                      child: Image.asset('images/logo.png'),
-                    ),
+      backgroundColor: animation.value,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: SizedBox(
+                    height: 60,
+                    child: Image.asset('images/logo.png'),
                   ),
-                  Text(
-                    'Flash Chat',
-                    style: TextStyle(
-                      fontSize: 45.0,
-                      fontWeight: FontWeight.w900,
-                      color: Colors.white,
-                    ),
+                ),
+                Text(
+                  'Flash chat',
+                  style: TextStyle(
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.white,
                   ),
-                ],
-              ),
-              SizedBox(height: 48.0),
+                ),
+              ],
+            ),
+            SizedBox(height: 48.0),
 
-              //  Expanded
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 12),
-                child: Material(
-                  elevation: 5.0,
-                  color: kLoginButtonColor,
-                  borderRadius: BorderRadius.circular(30.0),
-                  child: MaterialButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, LoginScreen.id);
-                    },
-                    minWidth: 200.0,
-                    height: 42.0,
-                    child: Text(
-                      'Log In',
-                      style: TextStyle(color: kWhiteColor),
-                    ),
+            //  Expanded
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              child: Material(
+                elevation: 5.0,
+                color: kLoginButtonColor,
+                borderRadius: BorderRadius.circular(30.0),
+                child: MaterialButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, LoginScreen.id);
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text(
+                    'Log In',
+                    style: TextStyle(color: kWhiteColor),
                   ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 12),
-                child: Material(
-                  elevation: 5.0,
-                  color: kRegisterButtonColor,
-                  borderRadius: BorderRadius.circular(30.0),
-                  child: MaterialButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, RegistrationScreen.id);
-                    },
-                    minWidth: 200.0,
-                    height: 42.0,
-                    child: Text(
-                      'Register',
-                      style: TextStyle(color: kWhiteColor),
-                    ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              child: Material(
+                elevation: 5.0,
+                color: kRegisterButtonColor,
+                borderRadius: BorderRadius.circular(30.0),
+                child: MaterialButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, RegistrationScreen.id);
+                  },
+                  minWidth: 200.0,
+                  height: 42.0,
+                  child: Text(
+                    'Register',
+                    style: TextStyle(color: kWhiteColor),
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
